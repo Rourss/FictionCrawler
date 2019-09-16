1d78130a052695c8c4b8162a77d75874e9342914
ROURSS <47970410+Rourss@users.noreply.github.com>
2019-09-16 16:27:59 +0800

    内部的基本构造

---
+++ FictionCrawler/MessageException/NoFound.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FictionCrawler.MessageException
+{
+    public class NoFound:Exception
+    {
+        public NoFound() //构造函数
+            : base("未找到符合的书籍") //调用父类构造函数
+        {
+
+        }
+        public NoFound(string message) // 构造函数
+            : base(message) //调用父类构造函数
+        {
+
+        }
+    }
+}
